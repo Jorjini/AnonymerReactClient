e9bf1733488b728bd79480e68242ca9529c8f169
ghveda <ghvedashvili1997@gmail.com>
2022-12-08 12:17:44 +0400

    Changes in Chat

--- src/Pages/Register/Register.component.tsx
+++ src/Pages/Register/Register.component.tsx
@@ -42,11 +42,7 @@ const Register = () => {
     const userData = JSON.parse(localStorage.getItem('userData')!);
     const userDataToken = userData?.token
 
-    if (token && !userDataToken?.emailVerified) {
-      navigate('/register/confirm-email');
-    } else if (token && userDataToken?.kycStatus === UserKycStatus.Pending) {
-      navigate('/kyc/upload');
-    } else if (
+    if (
       token &&
       userDataToken?.kycStatus === UserKycStatus.Approved &&
       userDataToken?.emailVerified
